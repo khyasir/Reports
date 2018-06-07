98aea362530a0349bc9494bd509e343739814409
khyasir <yasirkrauf@yahoo.com>
2018-06-07 16:54:57 +0500

    Odoo 10 SQL reports

---
+++ odoo_10_module_Git.py
@@ -0,0 +1,10 @@
+import os
+import time
+ 
+os.chdir('/home/odoo/Downloads/NitroShare/coomon_reports_yasir/')
+os.system('git init')
+os.system('git remote add origin https://github.com/khyasir/Reports.git')
+os.system('git add *')
+os.system('git status')
+os.system('git commit -m "Odoo 10 SQL reports"')
+os.system('git push origin master')
